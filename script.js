16b56a0b50cc6a920faf9c485af0bd3a8893935b
HendrikCromboom <hcromboom@gmail.com>
2020-09-12 14:05:58 +0200

    JS pull added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Let's do this!
+var thisForm = {};
+function getId(id) { return document.getElementById(id); }
+function getValue(id) { return getId(id).value; }
+function formData(id) { thisForm[id] = getValue(id); }
+function formSubmit() {
+    pullFormData();
+}
+function pullFormData() {
+    formData("bookTitle");
+    formData("bookAuthor");
+    formData("bookRead");
+    formData("bookStatus");
+    console.log(thisForm);
+}
--- script.ts
+++ script.ts
@@ -1,1 +1,18 @@
-// Let's do this!
+let thisForm = {}
+function getId(id){ return <HTMLInputElement> document.getElementById(id)}
+function getValue(id){ return getId(id).value}
+function formData(id){thisForm[id] = getValue(id)}
+
+
+function formSubmit(){
+    pullFormData()
+}
+function pullFormData(){
+    formData("bookTitle")
+    formData("bookAuthor")
+    formData("bookRead")
+    formData("bookStatus")
+    console.log(thisForm)
+
+
+}
