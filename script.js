c5f8d3f6cc0d1884abf5615231bbcca643372526
HendrikCromboom <hcromboom@gmail.com>
2020-09-12 16:20:35 +0200

    made file ts compliant

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var thisForm = {};
+var thisForm = {
+    bookTitle: "",
+    bookAuthor: "",
+    bookRead: "",
+    bookStatus: ""
+};
 function getId(id) { return document.getElementById(id); }
 function getValue(id) { return getId(id).value; }
 function formData(id) { return thisForm[id] = getValue(id); }
 function formSubmit() {
     pullFormData();
     console.log(thisForm);
+    pushFormToTable();
 }
 function pullFormData() {
     formData("bookTitle");
@@ -14,5 +20,15 @@ function pullFormData() {
 }
 function pushFormToTable() {
     var table = getId("outputTable").getElementsByTagName("tbody")[0];
-    var newRow = table.insertRow;
+    var newRow = table.insertRow();
+    var cell1 = newRow.insertCell(0);
+    cell1.innerHTML = thisForm.bookTitle;
+    var cell2 = newRow.insertCell(1);
+    cell2.innerHTML = thisForm.bookAuthor;
+    var cell3 = newRow.insertCell(2);
+    cell3.innerHTML = thisForm.bookRead;
+    var cell4 = newRow.insertCell(3);
+    cell4.innerHTML = thisForm.bookStatus;
+    var cell5 = newRow.insertCell(4);
+    cell5.innerHTML = "<a onClick=\"onEdit(this)\">Edit</a>\n                       <a onClick=\"onDelete(this)\">Delete</a>";
 }
--- script.ts
+++ script.ts
@@ -1,4 +1,15 @@
-let thisForm = {};
+let thisForm:{
+    bookTitle: string,
+    bookAuthor: string,
+    bookRead: string,
+    bookStatus: string,
+} = {
+    bookTitle: "",
+    bookAuthor: "",
+    bookRead: "",
+    bookStatus:"",
+};
+
 function getId(id){ return document.getElementById(id)}
 function getValue(id){ return (<HTMLInputElement>getId(id)).value}
 function formData(id){ return thisForm[id] = getValue(id)}
@@ -7,6 +18,8 @@ function formData(id){ return thisForm[id] = getValue(id)}
 function formSubmit(){
     pullFormData();
     console.log(thisForm);
+    pushFormToTable()
+
 }
 function pullFormData(){
     formData("bookTitle");
@@ -16,5 +29,16 @@ function pullFormData(){
 }
 function pushFormToTable(){
     let table = getId("outputTable").getElementsByTagName("tbody")[0];
-    let newRow = (<HTMLTableSectionElement>table).insertRow
+    let newRow = (<HTMLTableSectionElement>table).insertRow()
+    let cell1 = newRow.insertCell(0);
+    cell1.innerHTML = thisForm.bookTitle;
+    let cell2 = newRow.insertCell(1);
+    cell2.innerHTML = thisForm.bookAuthor;
+    let cell3 = newRow.insertCell(2);
+    cell3.innerHTML = thisForm.bookRead;
+    let cell4 = newRow.insertCell(3);
+    cell4.innerHTML = thisForm.bookStatus;
+    let cell5 = newRow.insertCell(4);
+    cell5.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
 }
