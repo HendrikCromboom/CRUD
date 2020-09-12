e30f83dc7c3f7e97a7b88f772d8336c58f35b814
HendrikCromboom <hcromboom@gmail.com>
2020-09-12 14:30:27 +0200

    backup lunch

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var thisForm = {};
 function getId(id) { return document.getElementById(id); }
 function getValue(id) { return getId(id).value; }
-function formData(id) { thisForm[id] = getValue(id); }
+function formData(id) { return thisForm[id] = getValue(id); }
 function formSubmit() {
     pullFormData();
+    console.log(thisForm);
 }
 function pullFormData() {
     formData("bookTitle");
     formData("bookAuthor");
     formData("bookRead");
     formData("bookStatus");
-    console.log(thisForm);
+}
+function pushFormToTable() {
+    var table = getId("outputTable").getElementsByTagName("tbody")[0];
+    var newRow = table.insertRow;
 }
--- script.ts
+++ script.ts
@@ -1,18 +1,20 @@
-let thisForm = {}
-function getId(id){ return <HTMLInputElement> document.getElementById(id)}
-function getValue(id){ return getId(id).value}
-function formData(id){thisForm[id] = getValue(id)}
+let thisForm = {};
+function getId(id){ return document.getElementById(id)}
+function getValue(id){ return (<HTMLInputElement>getId(id)).value}
+function formData(id){ return thisForm[id] = getValue(id)}
 
 
 function formSubmit(){
-    pullFormData()
+    pullFormData();
+    console.log(thisForm);
 }
 function pullFormData(){
-    formData("bookTitle")
-    formData("bookAuthor")
-    formData("bookRead")
-    formData("bookStatus")
-    console.log(thisForm)
-
-
+    formData("bookTitle");
+    formData("bookAuthor");
+    formData("bookRead");
+    formData("bookStatus");
+}
+function pushFormToTable(){
+    let table = getId("outputTable").getElementsByTagName("tbody")[0];
+    let newRow = (<HTMLTableSectionElement>table).insertRow
 }
